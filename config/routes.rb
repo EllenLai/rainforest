3003459f21f1647d300effcb2de95215063b7417
EllenLai <miss.ellenlai@gmail.com>
2014-02-04 16:42:58 -0500

    Adding flash alerts and notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rainforest::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "users/new"
   get "users/create"
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
