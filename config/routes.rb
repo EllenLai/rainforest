e6c65a854e212c75b2850d3ce9533b5aa1b162d2
EllenLai <miss.ellenlai@gmail.com>
2014-02-04 13:02:46 -0500

    Generating the producs controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rainforest::Application.routes.draw do
+  resources :products
+  root :to => "pictures#index"
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,3 @@ Rainforest::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
